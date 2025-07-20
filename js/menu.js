80be407e3d65069e6bda33bfb1ba11a96ee2d364
kevi2310 <kevissen.nullatamby@gmail.com>
2025-07-20 19:15:38 +0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,32 @@
 $(document).ready(function () {
-  // Toggle sidebar menu and body scroll
-  $('.icon').click(function () {
-    $('nav ul.responsive').toggleClass('active');
-    $('body').toggleClass('no-scroll');
+  $('.icon').on('click keydown', function (e) {
+    if (e.type === 'click' || e.key === 'Enter' || e.key === ' ') {
+      e.preventDefault();
+      const isActive = $('nav ul.responsive').toggleClass('active').hasClass('active');
+      $('body').toggleClass('no-scroll');
+      $(this).attr('aria-expanded', isActive);
+    }
   });
 
-  // Close menu when a nav link is clicked
   $('nav ul.responsive li a').click(function () {
     $('nav ul.responsive').removeClass('active');
     $('body').removeClass('no-scroll');
+    $('.icon').attr('aria-expanded', false);
   });
 
-  // Close menu with Escape key
   $(document).keyup(function (e) {
     if (e.key === "Escape") {
       $('nav ul.responsive').removeClass('active');
       $('body').removeClass('no-scroll');
+      $('.icon').attr('aria-expanded', false);
+    }
+  });
+
+  $(document).click(function(e) {
+    if (!$(e.target).closest('nav').length && $('nav ul.responsive').hasClass('active')) {
+      $('nav ul.responsive').removeClass('active');
+      $('body').removeClass('no-scroll');
+      $('.icon').attr('aria-expanded', false);
     }
   });
 });
